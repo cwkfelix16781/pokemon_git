a060799f7f687a5c35407c72dbb3479716654547
Felix Norton <xelifnorton@gmail.com>
2025-11-25 18:22:30 -0800

    basic changes

--- pokemon.py
+++ pokemon.py
@@ -1,2 +1,11 @@
 # some code
 print('hello')
+print('Code is intresting')
+print('its also hard')
+print('but fun')
+print('lets keep going')
+print('almost done')
+print('finished!')
+print('Just added a new line')
+print('This is another line added later')
+print('goodbye!')
